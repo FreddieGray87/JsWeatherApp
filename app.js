75275654177f88f7a3a689064002516fb8bbca54
Fred Graybeal <fred.c.graybeal@gmail.com>
2019-07-10 01:39:44 -0400

    Added Js

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-window.addEventListener('load', ()=>{
+window.addEventListener("load", ()=> {
     let long;
     let lat;
-
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
         long = position.coords.longitude;
         lat = position.coords.latitude;
+        
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        const api = `${proxy}https://api.darksky.net/forecast/10959b014ffd98c692a77ece6aef6092/${lat},${long}`;
+        
+        fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const { temperature, summary, icon} = data.currently;
+            // Set DOM Elements from API
+            temperatureDegree.textContent = temperature;
+            temperatureDescription.textContent = summary;
+            locationTimezone.textContent = data.timezone;
+                //Set Icon
+                setIcons(icon, document.querySelector('.icon'));
+            });
         });
     } 
+    
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 });
